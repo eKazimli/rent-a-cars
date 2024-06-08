dd19260ec68c520ccde510b00af8131534998fe1
eKazimli <elmankazimli7@gmail.com>
2024-06-08 19:32:47 +0400

    Users..

--- src/main/java/org/example/rentacar/Launcher.java
+++ src/main/java/org/example/rentacar/Launcher.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RentACarApplication {
-
+public class Launcher {
 	public static void main(String[] args) {
-		SpringApplication.run(RentACarApplication.class, args);
+		SpringApplication.run(Launcher.class, args);
 	}
-
 }
--- src/main/java/org/example/rentacar/database/contact/controller/skksskskscs.java
+++
@@ -1,4 +0,0 @@
-package org.example.rentacar.database.contact.controller;
-
-public class skksskskscs {
-}
